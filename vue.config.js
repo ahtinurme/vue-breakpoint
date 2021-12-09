9862f08a968ca6b7c1708aa00f8fd84c17bdfb62
Ahti Nurme <ahtinurme@gmail.com>
2021-12-09 14:22:50 +0200

    Update deps

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const { resolveExternals } = require('monorepo-utils')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   chainWebpack: (config) => {
     // https://stackoverflow.com/a/45820235/4106263
-    config.externals(resolveExternals({ importType: 'umd' }))
+    config.externals(nodeExternals({ importType: 'umd' }))
   },
   pluginOptions: {
     jestSerializer: {
